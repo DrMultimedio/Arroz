03db2c007e48c442051a182e63ca9f34ef67076c
coveromaki <coveromaki@gmail.com>
2016-10-17 17:47:41 +0200

    Aplicación completada
    Falta una mijita de diseño y cariño

--- funciones.js
+++ funciones.js
@@ -17,7 +17,13 @@ function EmpezarReloj(duration, display) {
 $( document ).ready(function() {
   $(function(){
       $('#boton_fuerte').click(function() {
-          EmpezarReloj(5,document.querySelector('#fuegofuerte'))
+          EmpezarReloj(60,document.querySelector('#fuegofuerte'))
+      });
+      $('#boton_flojo').click(function() {
+          EmpezarReloj(360,document.querySelector('#fuegoflojo'))
+      });
+      $('#boton_reposar').click(function() {
+          EmpezarReloj(600,document.querySelector('#reposar'))
       });
   });
 });
